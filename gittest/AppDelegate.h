304086cf56931a96ccc125b65dfd184d69118a07
headligt <junekim@juneui-macbook-air.local>
2016-11-10 11:38:37 +0900

    Initial Commit

---
+++ gittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittest
+//
+//  Created by june kim on 2016. 11. 10..
+//  Copyright © 2016년 june kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gittest
+//
+//  Created by june kim on 2016. 11. 10..
+//  Copyright © 2016년 june kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
